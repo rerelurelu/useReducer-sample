628c39ee0818a473a8a29f3940e60e1ccfa53b8c
zoniha <awaawapurin@gmail.com>
2022-09-23 15:40:39 +0900

    refactor: Move Post to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,40 @@
-import { Post } from './Post';
+import { useState } from 'react';
 
-function App() {
+type Post = {
+  userId?: string;
+  id?: string;
+  title?: string;
+  body?: string;
+};
+
+export const App = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [post, setPost] = useState<Post>({});
+  const [error, setError] = useState<boolean>(false);
+
+  const handleFetch = () => {
+    setLoading(true);
+    setError(false);
+
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setPost(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(true);
+        setLoading(false);
+      });
+  };
+
   return (
     <>
-      <Post />
+      <button onClick={handleFetch}>{loading ? 'データ取得中...' : 'データを取得する'}</button>
+      <p>{post?.title}</p>
+      <span>{error && 'エラーが発生しました'}</span>
     </>
   );
-}
-
-export default App;
+};
